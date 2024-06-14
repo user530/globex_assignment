8f2d2b5c2e13885cd59c92f8203ab9b19a3f35ec
user530 <m.timarsuev@gmail.com>
2024-06-14 19:28:49 +0300

    Added main theme

---
+++ src/styles/themes/MainTheme.ts
@@ -0,0 +1,23 @@
+export const MainTheme = {
+    colors: {
+        white: '#FFF',
+        black: '#000',
+        // Blueberry
+        accent: '#D4DEFE',
+        // Plumbum
+        textMain: '#262C40',
+        // Asphalt 
+        textLight: '#8189A3',
+        // PurpleFog
+        icons: '#432EAB',
+    },
+    fonts: {
+        main: 'Nova Regular',
+        bold: 'Nova Bold',
+    },
+    spacing: {
+        small: '0.5rem',
+        medium: '1rem',
+        large: '2rem',
+    },
+}
