c9f9482af525b1c8922277662d5461a249ffbbbd
user530 <m.timarsuev@gmail.com>
2024-06-15 14:22:40 +0300

    Add shared container component

---
+++ src/styles/shared/Container.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: min(100%, ${`${1120 + 15 * 2}px`});
+    padding-inline: 15px;
+    margin-inline: auto;
+`;
