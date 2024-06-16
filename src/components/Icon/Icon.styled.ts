aba8aeb8372e9b01e591aa81dd8531f58c8be87e
user530 <m.timarsuev@gmail.com>
2024-06-16 11:42:25 +0300

    Fixed a bug with icon styled component

--- src/components/Icon/Icon.styled.ts
+++ src/components/Icon/Icon.styled.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledIcon = styled.svg`
-    width: ${({ width, theme }) => width || theme.spacing.medium};
-    height: ${({ height, theme }) => height || theme.spacing.medium};
+    width: ${({ width, theme }) => `${width}px` || theme.spacing.medium};
+    height: ${({ height, theme }) => `${height}px` || theme.spacing.medium};
     color: ${({ fill }) => fill || 'currentColor'};
 `;
